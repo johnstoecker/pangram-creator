e55040634a84236be243187756a130a59af4a6f8
John Stoecker <johnstoecker@gmail.com>
2017-02-05 12:12:26 -0800

    rename panagram (sic) to pangram

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Panagram: a sentence with every letter in the alphabet. Create your own!
+pangram: a sentence with every letter in the alphabet. Create your own!
--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
-angular.module('panagramCreator', [])
-  .controller('PanagramCreatorController', function($scope) {
+angular.module('pangramCreator', [])
+  .controller('pangramCreatorController', function($scope) {
 
     $scope.wordBank = [];
     $scope.allLetters = [];
@@ -8,16 +8,16 @@ angular.module('panagramCreator', [])
     }
     $scope.wordBank = $scope.allLetters;
 
-    $scope.$watch('panagram', function (newValue, oldValue, scope) {
+    $scope.$watch('pangram', function (newValue, oldValue, scope) {
       if(newValue != undefined) {
-        lowerCasePanagram = newValue.toLowerCase();
+        lowerCasepangram = newValue.toLowerCase();
         tempWordBank = []
         for(i = 97; i <=122; i++ ) {
-          if(lowerCasePanagram.indexOf(String.fromCharCode(i)) == -1) {
+          if(lowerCasepangram.indexOf(String.fromCharCode(i)) == -1) {
             tempWordBank.push(String.fromCharCode(i))
           }
         }
-        console.log(lowerCasePanagram);
+        console.log(lowerCasepangram);
         $scope.wordBank = tempWordBank;
       }
     });
