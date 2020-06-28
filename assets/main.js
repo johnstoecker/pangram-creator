7c3352bc95898b63a7aef6dcc499f31673048723
John Stoecker <johnstoecker@gmail.com>
2020-06-28 15:18:08 -0700

    pangram used letter count, responsive width

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,7 @@ angular.module('pangramCreator', [])
   .controller('pangramCreatorController', function($scope) {
 
     $scope.wordBank = [];
+    $scope.usedLetterCount = {};
     $scope.allLetters = [];
     for(i = 97; i <= 122; i++ ) {
       $scope.allLetters.push(String.fromCharCode(i))
@@ -12,13 +13,25 @@ angular.module('pangramCreator', [])
       if(newValue != undefined) {
         lowerCasepangram = newValue.toLowerCase();
         tempWordBank = []
+        tempUsedLetterCounts = {}
         for(i = 97; i <=122; i++ ) {
           if(lowerCasepangram.indexOf(String.fromCharCode(i)) == -1) {
             tempWordBank.push(String.fromCharCode(i))
           }
         }
-        console.log(lowerCasepangram);
+        usedLetters = lowerCasepangram.split("").sort();
+        for(i=0; i< usedLetters.length; i++) {
+          if(scope.allLetters.indexOf(usedLetters[i]) !== -1) {
+            currentLetter = usedLetters[i];
+            if(tempUsedLetterCounts[currentLetter] == null) {
+              tempUsedLetterCounts[currentLetter] = 1;
+            } else {
+              tempUsedLetterCounts[currentLetter] += 1;
+            }
+          }
+        }
         $scope.wordBank = tempWordBank;
+        $scope.usedLetterCount = tempUsedLetterCounts;
       }
     });
     $("#pangrammer")[0].focus()
